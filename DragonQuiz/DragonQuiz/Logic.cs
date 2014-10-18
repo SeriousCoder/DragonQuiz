21fc2a92a42deddd7013f971218b0e08e7af9c9e
SeriousCoder <0kintol0@gmail.com>
2014-10-18 17:51:36 +0400

    Revert "Revert "WPF client added""
    This reverts commit bd6ca8db1521740bf6bcbe91304effe937c98c50.

---
+++ DragonQuiz/DragonQuiz/Logic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+class Integration
+{
+    public class DQuestion
+    {
+        public int ID{ get; set; }
+        public string Content{ get; set; }
+        public string Answer{ get; set; }
+        public string Comment{ get; set; }
+        public string Tags{ get; set; }
+    };
+
+    public struct Request
+    {
+        public int Number;
+        public string Tags;
+        public void setRequest(int n, string tags)
+        {
+            Number = n;
+            Tags = tags;
+        }
+    };
+
+
+    public static List<DQuestion> getPackage(Request request)
+    {
+
+        List<DQuestion> l = new List<DQuestion>();
+        for (int i = 1; i <= request.Number; ++i)
+            {        DQuestion q = new DQuestion();
+        q.Content = "aaa";
+        q.Answer = "tt";
+        q.Tags = "#azaza";
+                l.Add(q);
+            }
+        return l;
+    }
+
+
+}
